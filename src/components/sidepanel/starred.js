8cf8c6a76b6db40d8eccb72f92c85feb292ae3fd
nshoukat <nouman.shoukat586@gmail.com>
2020-06-20 00:11:35 +0500

    show list of fav channels

--- src/components/sidepanel/starred.js
+++ src/components/sidepanel/starred.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from '../../firebase';
 import {Menu,Icon} from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import {setCurrentChannel, setPrivateChannel} from '../../actions';
@@ -9,9 +10,40 @@ class Starred extends React.Component{
     constructor(props){
         super(props);
         this.state= {
+            user: this.props.currentUser,
+            userRef: firebase.database().ref('users'),
             starredChannels: [],
             activeChannel: '',
         }
+    }
+
+    componentDidMount(){
+        if(this.state.user){
+            this.addListeners(this.state.user.uid);
+        }
+    }
+
+    addListeners= userId => {
+        this.state.userRef
+            .child(userId)
+            .child('starred')
+            .on('child_added', snap => {
+                const starredChannel= { id: snap.key, ...snap.val() };
+                this.setState({
+                    starredChannels: [...this.state.starredChannels, starredChannel]
+                });
+            });
+
+        this.state.userRef
+            .child(userId)
+            .child('starred')
+            .on('child_removed', snap => {
+                const channelToRemove= { id: snap.key, ...snap.val() };
+                const filteredChannels= this.state.starredChannels.filter(channel => {
+                    return channel.id !== channelToRemove.id;
+                });
+                this.setState({starredChannels: filteredChannels});
+            });
     }
 
     setActiveChannel= channel => {
